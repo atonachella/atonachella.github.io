90b1951a21b875ecb24fe87aea4a2720753cb5ed
Adam Tonachella <adam.tonachella@gmail.com>
2026-05-28 18:06:47 -0700

    fullscreen

--- screensavervirus/app.js
+++ screensavervirus/app.js
@@ -186,6 +186,13 @@ window.addEventListener('resize', () => {
     }
 });
 
+// This forces DecSoft HTML Compiler to maximize full screen on startup
+if (typeof dhc !== 'undefined') {
+    // Tells the compiler window to maximize immediately
+    dhc.command('maximize', ''); 
+}
+Step 2: Kill the Scrollbars (CSS)
+
 // Close the screensaver on any mouse movement or keypress
 window.addEventListener('mousemove', () => window.close());
 window.addEventListener('keydown', () => window.close());
